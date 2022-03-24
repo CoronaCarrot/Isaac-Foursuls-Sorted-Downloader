14f990c21c736b5f0ff1e9d161b2b819569753f8
Carrot <tdog375@outlook.com>
2022-03-24 13:23:51 +0000

    added commenting :P

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ from selenium import webdriver
 from tqdm import tqdm
 from termcolor import colored
 
+# Added to ignore Deprecation Warnings because I can't be bothered to switch to new system
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# Checks if the Downloads folder exists
 if not os.path.exists("./Downloads"):
     os.mkdir("./Downloads")
 
+# Sets the user agent
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+# Defines options for the headless Chrome browser
 options = webdriver.ChromeOptions()
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--headless")
@@ -23,7 +27,9 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--allow-running-insecure-content')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 browser = webdriver.Chrome(options=options)
+
 try:
+    # CLI Title
     print(colored("""
 █████████""", "red") + colored("""  THE BINDING OF
 """, "yellow") + colored("""   ███   █████   ██████    ██████    ██████                
@@ -39,6 +45,7 @@ https://github.com/CoronaCarrot/Isaac-Foursuls-Sorted-Downloader""", "white"))
         "\nwith the cards you wish to download.\n")
     loop = True
     while loop:
+        # Await valid link input
         web = input("> ")
         if web.startswith("https://foursouls.com/card-search/?"):
             loop = False
@@ -47,14 +54,20 @@ https://github.com/CoronaCarrot/Isaac-Foursuls-Sorted-Downloader""", "white"))
         else:
             print("Invalid link, please try again.")
 
+    # generates a download file for the current download
     filename = str(uuid.uuid1())
     os.mkdir(f"./Downloads/{filename}")
 except:
     exit()
 
+# Finds the card count
 cardcount = browser.find_element_by_xpath("//*[contains(text(), 'cards found')]")
+
+# gets page amount
 pages = browser.find_elements_by_xpath("//*[contains(@class, 'page-numbers')]")
 prev = 0
+
+# gets a list of all card links within those pages
 for page in pages:
     if int(page.text) > prev:
         prev = int(page.text)
@@ -80,6 +93,7 @@ while prev > 0:
             cardhref.append(href)
     time.sleep(1)
 
+# runs through the full list of cards to download
 for card in tqdm(cardhref, bar_format="{l_bar}{bar}|{n_fmt}/{total_fmt} [Est. {remaining}]"):
     browser.get(card)
     try:
@@ -116,11 +130,14 @@ for card in tqdm(cardhref, bar_format="{l_bar}{bar}|{n_fmt}/{total_fmt} [Est. {r
     imgname = card.split("/")
     imgname = imgname[len(imgname) - 2]
 
+    # checks if the category exists, if not it creates it
     if not os.path.exists(f"Downloads/{filename}/{type}"):
         os.mkdir(f"Downloads/{filename}/{type}")
+    # saves card to folder
     with open(f'Downloads/{filename}/{type}/{imgname}.png', 'wb') as f:
         f.write(requests.get(url.replace("-308x420", "")).content)
         f.close()
 
+# zips all cards into an archive
 print("\nDownload complete. Zipping files...")
 make_archive(f'./Downloads/{filename}/Output', 'zip', root_dir=f'./Downloads/{filename}')
